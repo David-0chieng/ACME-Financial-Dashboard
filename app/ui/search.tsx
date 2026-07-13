4431a9c91d571aefc1b96fcd4635b73d0e424998
David Ochieng <212739230+David-0chieng@users.noreply.github.com>
2026-07-13 23:16:55 +0300

    edit search on the invoice page

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,13 +9,15 @@ export default function Search() {
   const pathname = usePathname();
 	const { replace } = useRouter();
 
-	function handleSearch(term: string) {
+  function handleSearch(term: string) {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     if (term) {
 			params.set("query", term);
 		} else {
 			params.delete("query");
-		}
+    }
+    replace(`${pathname}?${params.toString()}`);
 	}
 	return (
 		<div className="relative flex flex-1 flex-shrink-0">
@@ -24,10 +26,11 @@ export default function Search() {
 			</label>
 			<input
 				className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-				placeholder={placeholder}
+				// placeholder={placeholder}
 				onChange={(e) => {
 					handleSearch(e.target.value);
 				}}
+				defaultValue={searchParams.get("query")?.toString()}
 			/>
 			<MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
 		</div>
