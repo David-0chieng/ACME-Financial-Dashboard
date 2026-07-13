7c4873d984db386e49e92b2a741d1a78a3d40f12
David Ochieng <212739230+David-0chieng@users.noreply.github.com>
2026-07-13 18:44:08 +0300

    edit navigation links

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,28 +1,30 @@
+"use client";
 import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+	UserGroupIcon,
+	HomeIcon,
+	DocumentDuplicateIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+	{ name: "Home", href: "/dashboard", icon: HomeIcon },
+	{
+		name: "Invoices",
+		href: "/dashboard/invoices",
+		icon: DocumentDuplicateIcon,
+	},
+	{ name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
-  return (
-    <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
+	return (
+		<>
+			{links.map((link) => {
+				const LinkIcon = link.icon;
+				return (
 					<Link
 						key={link.name}
 						href={link.href}
@@ -31,7 +33,7 @@ export default function NavLinks() {
 						<p className="hidden md:block">{link.name}</p>
 					</Link>
 				);
-      })}
-    </>
-  );
+			})}
+		</>
+	);
 }
